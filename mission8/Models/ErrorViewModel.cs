eaa450f579a40bc637aad1598fc91f573218a4b6
zachheaton <zacharymartinheaton@gmail.com>
2022-02-22 08:26:59 -0700

    very broken

---
+++ mission8/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace mission8.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
